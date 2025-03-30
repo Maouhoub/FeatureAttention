e799b1c3ff28aaf6bda65a2c21e361c84012bbbc
Maouhoub <ksmaouhoub@gmail.com>
2025-03-30 13:22:40 +0000

    add more command line arguments

--- model.py
+++ model.py
@@ -18,14 +18,15 @@ class SRDataset(Dataset):
         self.upscale_factor = upscale_factor
         self.transform_lr = transform_lr
         self.transform_hr = transform_hr
-        self.filenames = sorted(os.listdir(lr_dir))
+        self.lr_filenames = sorted(os.listdir(lr_dir))
+        self.hr_filenames = sorted(os.listdir(hr_dir))
 
     def __len__(self):
-        return len(self.filenames)
+        return len(self.hr_filenames)
 
     def __getitem__(self, idx):
-        lr_path = os.path.join(self.lr_dir, self.filenames[idx])
-        hr_path = os.path.join(self.hr_dir, self.filenames[idx])
+        lr_path = os.path.join(self.lr_dir, self.lr_filenames[idx])
+        hr_path = os.path.join(self.hr_dir, self.hr_filenames[idx])
         lr_img = Image.open(lr_path).convert("RGB")
         hr_img = Image.open(hr_path).convert("RGB")
         if self.transform_lr:
